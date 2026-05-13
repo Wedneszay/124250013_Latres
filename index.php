0e30eb099740c3b668b5f8e14d40b823906ae76b
Wedneszay <mazayafirasyan@gmail.com>
2026-05-13 23:16:16 +0700

    Update class in index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="stylepustaka.css">
 </head>
-<body>
+<body class="body-login">
     <main class="wrapper-form">
         <form action="proses_login.php" method="POST">
             <div class="form-login">
