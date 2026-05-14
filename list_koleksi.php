ca36f898f646cbbd907b4172ea0a4867accff9b1
Wedneszay <mazayafirasyan@gmail.com>
2026-05-14 20:58:21 +0700

    Merapikan kode

--- list_koleksi.php
+++ list_koleksi.php
@@ -185,7 +185,6 @@ $result = mysqli_query($koneksi, $query)
             } ?>
         </div>
     </main>
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
