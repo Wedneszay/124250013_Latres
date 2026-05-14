1ffb4f3da565c4eb9840cce2916bb1791fcc8937
Wedneszay <mazayafirasyan@gmail.com>
2026-05-14 19:20:43 +0700

    Menambahkan file php untuk memproses insert data buku

---
+++ proses_insert.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include 'connection.php';
+
+$kode_buku = $_POST['kodeBuku'];
+$stok = $_POST['stok'];
+$judul = $_POST['judul'];
+$pengarang = $_POST['pengarang'];
+$kategori = $_POST['kategori'];
+
+$cekKode = "SELECT * FROM databuku WHERE kode_buku = '$kode_buku'";
+$resultCek = mysqli_query($koneksi, $cekKode);
+
+if (mysqli_num_rows($resultCek) > 0) {
+    $_SESSION['insertGagal'] = "Insert gagal! Kode buku sudah digunakan.";
+    header('Location: koleksibuku.php');
+    exit();
+}
+else {
+    $queryInsert = "INSERT INTO databuku(id, kode_buku, judul, pengarang, kategori, stok) VALUES (' ','$kode_buku','$judul','$pengarang','$kategori','$stok')";
+    $resultInsert = mysqli_query($koneksi, $queryInsert);
+    if ($resultInsert) {
+        header('Location: koleksibuku.php');
+        exit();
+    }
+    else {
+        echo "Gagal menambah data: " . mysqli_error($koneksi);
+    }
+}
+?>
